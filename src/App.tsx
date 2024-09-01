23da6aa9687b4c9800202407285e82df60d21801
Panos Anastasiadis <anastapd@gmail.com>
2024-09-01 20:47:47 +0100

    build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
-import { Button } from '@chakra-ui/react';
+import { Button, Grid, GridItem, Show } from '@chakra-ui/react';
 
 function App() {
-    const [count, setCount] = useState(0);
+    return (
+        <Grid
+            templateAreas={{
+                base: `"nav" "main"`,
+                lg: `"nav nav" "aside main"`,
+            }}
+        >
+            <GridItem area="nav" bg="coral">
+                Nav
+            </GridItem>
+            <Show above="lg">
+                <GridItem area="aside" bg="gold">
+                    Aside
+                </GridItem>
+            </Show>
 
-    return <Button colorScheme="blue">Button</Button>;
+            <GridItem area="main" bg="green">
+                Main
+            </GridItem>
+        </Grid>
+    );
 }
 
 export default App;
